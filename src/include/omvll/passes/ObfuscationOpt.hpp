750abf0ebf55e029fec58a6ecf7ae7e4ddda5835
Dawn <152718018+LiMing-Dawn@users.noreply.github.com>
2026-08-07 14:21:50 +0800

    sync: update upstream src/include/omvll/passes/ObfuscationOpt.hpp

--- src/include/omvll/passes/ObfuscationOpt.hpp
+++ src/include/omvll/passes/ObfuscationOpt.hpp
@@ -8,6 +8,7 @@
 #include "omvll/passes/anti-hook/AntiHookOpt.hpp"
 #include "omvll/passes/arithmetic/ArithmeticOpt.hpp"
 #include "omvll/passes/basic-block-duplicate/BasicBlockDuplicateOpt.hpp"
+#include "omvll/passes/basic-block-split/BasicBlockSplitOpt.hpp"
 #include "omvll/passes/break-cfg/BreakControlFlowOpt.hpp"
 #include "omvll/passes/cfg-flattening/ControlFlowFlatteningOpt.hpp"
 #include "omvll/passes/function-outline/FunctionOutlineOpt.hpp"
@@ -15,4 +16,5 @@
 #include "omvll/passes/indirect-call/IndirectCallOpt.hpp"
 #include "omvll/passes/opaque-constants/OpaqueConstantsOpt.hpp"
 #include "omvll/passes/opaque-field-access/OpaqueFieldAccessOpt.hpp"
+#include "omvll/passes/shuffle-ops/ShuffleOpsOpt.hpp"
 #include "omvll/passes/string-encoding/StringEncodingOpt.hpp"
